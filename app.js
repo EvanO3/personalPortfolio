2b1f12987aa58324dc180c1a575e25318f7edd23
EvanO3 <Evan.oni@hotmail.com>
2023-09-03 18:07:46 -0400

    added scroll animation for about section

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 
 function scrollDown(){
-    const midPage = document.getElementById('about');
+    const midPage = document.getElementById('intro');
     midPage.scrollIntoView({behavior:'smooth'})
+}
+
+function scrollToAbout(){
+    const About = document.querySelector('.about')
+    About.scrollIntoView({behavior:'smooth'})
 }
