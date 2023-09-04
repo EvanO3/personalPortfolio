a142ff0529932f98db0c9ce9e2aa66c0cbac63a4
EvanO3 <Evan.oni@hotmail.com>
2023-09-03 23:21:14 -0400

    added slide to skills function and also added background change

--- app.js
+++ app.js
@@ -2,9 +2,15 @@
 function scrollDown(){
     const midPage = document.getElementById('intro');
     midPage.scrollIntoView({behavior:'smooth'})
+    document.body.style.backgroundColor ="#1F1A38"
 }
 
 function scrollToAbout(){
     const About = document.querySelector('.about')
     About.scrollIntoView({behavior:'smooth'})
+    document.body.style.backgroundColor ="#6665dd"
+}
+
+function scrollToSkills(){
+    
 }
