f8a532f754e1f850f8c986fd9dcea26709fce0f4
EvanO3 <Evan.oni@hotmail.com>
2023-09-04 02:03:55 -0400

    added scrolling for contact

--- app.js
+++ app.js
@@ -2,15 +2,22 @@
 function scrollDown(){
     const midPage = document.getElementById('intro');
     midPage.scrollIntoView({behavior:'smooth'})
-    document.body.style.backgroundColor ="#1F1A38"
+   // document.body.style.backgroundColor ="#1F1A38"
 }
 
 function scrollToAbout(){
     const About = document.querySelector('.about')
     About.scrollIntoView({behavior:'smooth'})
-    document.body.style.backgroundColor ="#6665dd"
+   // document.body.style.backgroundColor ="#6665dd"
 }
 
 function scrollToSkills(){
-    
+    const About = document.querySelector('.skills-Projects')
+    About.scrollIntoView({behavior:'smooth'})
+}
+
+
+function scrollToContact(){
+    const contact= document.querySelector('.contact')
+    contact.scrollIntoView({behavior:'smooth'})
 }
