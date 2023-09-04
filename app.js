8007da189fd69d0c6a94a89501bdf9271432976f
EvanO3 <Evan.oni@hotmail.com>
2023-09-04 02:05:19 -0400

    fixed code structure

--- app.js
+++ app.js
@@ -1,23 +1,21 @@
-
-function scrollDown(){
-    const midPage = document.getElementById('intro');
-    midPage.scrollIntoView({behavior:'smooth'})
-   // document.body.style.backgroundColor ="#1F1A38"
+function scrollDown() {
+  const midPage = document.getElementById("intro");
+  midPage.scrollIntoView({ behavior: "smooth" });
+  // document.body.style.backgroundColor ="#1F1A38"
 }
 
-function scrollToAbout(){
-    const About = document.querySelector('.about')
-    About.scrollIntoView({behavior:'smooth'})
-   // document.body.style.backgroundColor ="#6665dd"
+function scrollToAbout() {
+  const About = document.querySelector(".about");
+  About.scrollIntoView({ behavior: "smooth" });
+  // document.body.style.backgroundColor ="#6665dd"
 }
 
-function scrollToSkills(){
-    const About = document.querySelector('.skills-Projects')
-    About.scrollIntoView({behavior:'smooth'})
+function scrollToSkills() {
+  const About = document.querySelector(".skills-Projects");
+  About.scrollIntoView({ behavior: "smooth" });
 }
 
-
-function scrollToContact(){
-    const contact= document.querySelector('.contact')
-    contact.scrollIntoView({behavior:'smooth'})
+function scrollToContact() {
+  const contact = document.querySelector(".contact");
+  contact.scrollIntoView({ behavior: "smooth" });
 }
